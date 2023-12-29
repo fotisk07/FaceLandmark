fdc15c8c5126ff76c8efb06a765c4406d426c910
Fotis <fotiskapotos@gmail.com>
2023-12-29 22:47:50 +0100

    Implement Autoencoder

--- models.py
+++ models.py
@@ -20,6 +20,58 @@ class Baseline(nn.Module):
 
     
 
-    
+class Autoencoder(nn.Module):
+    def __init__(self, num_landmarks):
+        super(Autoencoder, self).__init__()
 
+        # Image (3, 256,256)
+        ### Encoder
+        # Conv 1 (16, 256, 256) kernel 7, padding same
+        # MaxPool 1 (10, 128, 128) 
+        # Conv 2 (32, 128, 128) kernel 5, padding same
+        # MaxPool 2 (32, 64, 64)
+        # Conv 3 (64, 64, 64) kernel 3, padding same
+        # MaxPool 3 (64, 32, 32)
+        # Conv 4 (128, 32, 32) kernel 1, padding same
+        # Max pool 4 (128, 16, 16) 
+
+        ### Decoder
+        # conv 5 (64, 32, 32)
+        # conv 6 (32, 64, 64)
+        # conv 7 (16, 128, 128)
+        # conv 8 (1, 256, 256)
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=7, padding="same"),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=5, padding="same"),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, padding="same"),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=1, padding="same"),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.decoder = nn.Sequential(
+            torch.nn.ConvTranspose2d(128,64, kernel_size=3, stride=2, padding=1,output_padding=1),
+            nn.ReLU(),
+            torch.nn.ConvTranspose2d(64,32, kernel_size=3, stride=2, padding=1,output_padding=1),
+            nn.ReLU(),
+            torch.nn.ConvTranspose2d(32,16, kernel_size=3, stride=2, padding=1,output_padding=1),
+            nn.ReLU(),
+            torch.nn.ConvTranspose2d(16,num_landmarks, kernel_size=3, stride=2, padding=1,output_padding=1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+        
+        
 
