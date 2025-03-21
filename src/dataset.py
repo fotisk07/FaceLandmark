28472dae4a553e20577b939b78659b54986364e1
Fotis <fotiskapotos@gmail.com>
2025-03-21 14:09:21 +0100

    Better Datasets

--- src/dataset.py
+++ src/dataset.py
@@ -1,121 +1,96 @@
 import os
+import xml.etree.ElementTree as ET
 import torch
-import pandas as pd
-from skimage import io, transform, filters
-import numpy as np
+import torchvision as tv
+from torchvision import tv_tensors
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
+import numpy as np
 
 
-class ToTensor(object):
-    def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
-        image = image.transpose((2, 0, 1))
-        return {'image': torch.from_numpy(image), 'landmarks': torch.from_numpy(landmarks)}
-    
-class ReduceLandmarks(object):
-    def __init__(self, step):
-        self.step = step
-
-    def __call__(self, sample):
-
-        landmarks = sample['landmarks']
-
-        landmarks = landmarks[::int(self.step)]
-
-
-        return {'image': sample['image'], 'landmarks': landmarks}
-    
-class Normalise(object):
-    def __call__(self, sample):
-        image, landmarks = sample['image'], sample['landmarks']
-        image = image.astype(np.float32)
-        image /= np.max(image)
-
-        return {'image': image, 'landmarks': landmarks}
-
-class FaceLandmarksDataset(Dataset):
-    """Face Landmarks dataset."""
-
-    def __init__(self, csv_file, root_dir,train=True, transform=None):
-        """
-        Arguments:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-                on a sample.
-        """
-        self.landmarks_frame = pd.read_csv(csv_file)
+class FaceDataset(torch.utils.data.Dataset):
+    def __init__(self, xml_file, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
+        self.data = self.parse_xml(xml_file)
 
-        if train:
-            self.landmarks_frame = self.landmarks_frame[:int(len(self.landmarks_frame)*0.8)]
-        else:
-            self.landmarks_frame = self.landmarks_frame[int(len(self.landmarks_frame)*0.8):]
+    def parse_xml(self, xml_file):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        data = []
+
+        for image in root.findall("images/image"):
+            img_path = os.path.join(
+                self.root_dir, "dlib_faces_5points", image.get("file")
+            )
+            # width, height = int(image.get("width")), int(image.get("height"))
+
+            for box in image.findall("box"):
+                bbox = [
+                    int(box.get("left")),
+                    int(box.get("top")),
+                    int(box.get("left")) + int(box.get("width")),
+                    int(box.get("top")) + int(box.get("height")),
+                ]
+                keypoints = {}
+                for part in box.findall("part"):
+                    keypoints[part.get("name")] = (
+                        int(part.get("x")),
+                        int(part.get("y")),
+                    )
+
+                data.append(
+                    {"img_path": img_path, "bbox": bbox, "keypoints": keypoints}
+                )
+
+        return data
 
     def __len__(self):
-        return len(self.landmarks_frame)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+        sample = self.data[idx]
+        image = tv.io.decode_image(sample["img_path"])
+        bbox = tv_tensors.BoundingBoxes(
+            sample["bbox"], canvas_size=image.shape[-2::], format="XYXY"
+        )
+        keypoints = torch.tensor(
+            list(sample["keypoints"].values()), dtype=torch.float32
+        )
 
-        img_name = os.path.join(self.root_dir,
-                                self.landmarks_frame.iloc[idx, 0] + ".jpg")
-        
-        image = io.imread(img_name)
-        landmarks = self.landmarks_frame.iloc[idx, 1:]
-        landmarks = np.array([landmarks], dtype=float).reshape(-1, 2)
-
-        
-        sample = {'image': image, 'landmarks': landmarks}
-     
         if self.transform:
-            sample = self.transform(sample)
+            image = self.transform(image)
+
+        return {"image": image, "bbox": bbox, "keypoints": keypoints}
 
 
-        landmarks = sample["landmarks"]
-        # Create the mask now that the landmarks are reduced
-        mask = np.zeros((len(landmarks), image.shape[0], image.shape[1]), dtype = "float32")
-        
-        for i in range(landmarks.shape[0]):
-            point = landmarks[i]
-            if 0 < point[1] < image.shape[0] and 0< point[0] < image.shape[1]:
-                mask[i , int(point[1]), int(point[0])] = 1
+def visualize_sample(sample):
+    image = sample["image"]
+    if isinstance(image, torch.Tensor):
+        image = tv.transforms.ToPILImage()(image)
 
-        mask = filters.gaussian(mask, sigma=10, channel_axis=0)
-        mask = mask / np.max(mask)
-        sample["mask"] = torch.from_numpy(mask)
+    # Convert image to numpy for visualization
+    image_np = np.array(image)
 
-        return sample
-    
+    # Draw bounding box
+    bbox = sample["bbox"][0].tolist()
 
+    # Plotting the image and bounding box
+    fig, ax = plt.subplots()
+    ax.imshow(image_np)
+    ax.add_patch(
+        plt.Rectangle(
+            (bbox[0], bbox[1]),
+            bbox[2] - bbox[0],
+            bbox[3] - bbox[1],
+            linewidth=2,
+            edgecolor="red",
+            facecolor="none",
+        )
+    )
 
-def create_data(csv_file, root_dir, batch_size=32, num_workers=1,validate=False,
-                transform=None, reduce_landmarks=20, only_valid=False):
-    
-    if not transform:
-        trans = transforms.Compose([ReduceLandmarks(reduce_landmarks), Normalise(), ToTensor()])
+    # Draw keypoints
+    for point in sample["keypoints"].tolist():
+        ax.plot(point[0], point[1], "bo")  # Blue circles for keypoints
 
-
-    train_data = FaceLandmarksDataset(csv_file=csv_file,root_dir=root_dir,train=True,
-                                      transform=trans)
-    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, 
-                              num_workers=num_workers)
-
-    valid_data = FaceLandmarksDataset(csv_file=csv_file,root_dir=root_dir,train=False,
-                                    transform=trans)
-
-    valid_loader = DataLoader(valid_data, batch_size=batch_size, shuffle=False,
-                              num_workers=num_workers)
-
-    if only_valid:
-        valid_loader = DataLoader(valid_data, batch_size=batch_size, shuffle=True,
-                              num_workers=num_workers)
-        train_loader = None
-    
-
-    return train_loader, valid_loader
-
+    ax.axis("off")
+    return fig, ax
